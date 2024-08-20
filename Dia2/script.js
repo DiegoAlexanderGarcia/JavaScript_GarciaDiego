47b2f02c53fcbdd9dd58616513b6a72960418c4f
DiegoGarcia <dgarciarodriguez370@gmail.com>
2024-08-20 12:55:03 -0500

    heat: :sparkles: code folder dia2

---
+++ Dia2/script.js
@@ -0,0 +1,16 @@
+console.log("\nopcion1\nopcion2\nopcion3\nopcion4");
+
+var mn =("menu \n");
+mn += ("op1 \n ops2\n");
+mn += ("op2\n");
+mn += ("op3\n");
+
+
+
+do{
+
+    var n = parseInt(prompt(mn));
+document.write("hola"+n);
+
+}while(n!=3);
+
