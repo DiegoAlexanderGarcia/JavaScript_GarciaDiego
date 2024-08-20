a6aef4a913bf527576a9aaecdb9499876ad940cb
DiegoGarcia <dgarciarodriguez370@gmail.com>
2024-08-20 09:29:55 -0500

    feat: :sparkles: new code creation

--- Dia1/script.js
+++ Dia1/script.js
@@ -8,3 +8,18 @@ function fahreheit(a){
     return console.log(32+(9*a/5))
 }
 fahreheit(18)
+
+function descuentos(a){
+    var b 
+    if (a<=20) {
+        var b = "30%"
+    } 
+    else if (a<=50) {
+        var b = "10%"
+    }
+    else {
+        var b = "no tienes desacuento"
+    }
+    return b
+}
+console.log(descuentos(10))
